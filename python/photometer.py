f40c417dd2ec1f60524d31e1947060415e8531ff
cmccully <cmccully@physics.rutgers.edu>
2015-06-05 16:22:29 -0700

    Initial layout.

--- python/photometer.py
+++ python/photometer.py
@@ -1,1 +1,16 @@
 __author__ = 'cmccully'
+
+# For an individual image
+
+# Get the reference catalog (include positional uncertainties)
+
+# Get Photometer targets
+
+# Run an emcee loop to get distributions of photometry
+
+
+###########################
+
+# Likelihood function
+
+#
--- python/preprocess.py
+++ python/preprocess.py
@@ -1,1 +1,16 @@
 __author__ = 'cmccully'
+
+# Collate image meta data
+
+# Create bad pixel masks
+    # cosmic rays
+    # Shutter issues
+
+# Create a noise model
+    # Estimate the read noise
+    # Use the read noise and the image data to make a noise map
+    # TODO Flag strong background gradients
+
+# Generate reference catalogs
+
+# Solve for WCS tweak
