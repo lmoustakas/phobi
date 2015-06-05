0591d94eeb80bffe6a4d6f360d01f5bb7d54e835
cmccully <cmccully@physics.rutgers.edu>
2015-06-05 16:05:12 -0700

    Initial structure setup.

---
+++ python/phobi.py
@@ -0,0 +1,11 @@
+__author__ = 'cmccully'
+
+def main():
+    # Pre-process images
+    # Photometer image
+    # Make light curve
+    # Infer Time delay
+    return
+
+if __name__ == '__main__':
+    main()
