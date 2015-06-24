ebb300f6db37ae6d7872de718797a41e1b3fe617
cmccully <cmccully@physics.rutgers.edu>
2015-06-24 14:41:51 -0700

    Added meta data list in the comments.

--- python/preprocess.py
+++ python/preprocess.py
@@ -1,6 +1,7 @@
 __author__ = 'cmccully'
 
 # Collate image meta data
+    # Filename, MJD, FILTER, EXPTIME, RA0, DEC0
 
 # Create bad pixel masks
     # cosmic rays
@@ -17,3 +18,5 @@ __author__ = 'cmccully'
     # fit each star in the catalogue to a Moffat
     # get the image centroids and Moffat parameters.
     # can scale subimages according to the Moffat FWHM
+
+
