d975f52958663d02a99887a5f42af65c62c9c5e9
afromero <rw.andres@gmail.com>
2015-08-10 13:03:47 -0700

    limited number of trials for estimating the read noise

--- python/preprocess.py
+++ python/preprocess.py
@@ -37,6 +37,10 @@ def produce_metadata(inputs):
 	    row.append(hdulist[0].header['DEC'])
 	    metadata.add_row(row)
 	    hdulist.close()
+	if(len(fnm)==0):  # ignore blank lines
+		continue
+	if('#' in fnm[0]): # ignore commented lines
+		continue
         if(not os.path.isfile(fnm)):
             print 'WARNING, THE FOLLOWING FILE DOES NOT EXIST:', fnm
     metadata.sort(['MJD-OBS'])
@@ -61,6 +65,7 @@ def loop(metadata_table, inputs):
     # initialize pre-process loop outputs
     metadata_table['num_masked_pixels']=-1
     metadata_table['read_noise']=-1.0
+    metadata_table['read_noise'].format = '%1.2f'
 
     # loop through image files
     for table_index in range(0,len(metadata_table)):
@@ -144,101 +149,119 @@ def rms(array):
 	return np.sqrt( np.mean(array**2) - np.mean(array)**2 )
 
 def estimate_read_noise(hdulist, bigw, Npx = 31, display=False, out = 'out'):
-	# get valid image limits
-	xmin = int((hdulist[0].header['TRIMSEC'])[1:-1].split(',')[0].split(':')[0])
-	xmax = int((hdulist[0].header['TRIMSEC'])[1:-1].split(',')[0].split(':')[1])
-	ymin = int((hdulist[0].header['TRIMSEC'])[1:-1].split(',')[1].split(':')[0])
-	ymax = int((hdulist[0].header['TRIMSEC'])[1:-1].split(',')[1].split(':')[1])
-	# initialize the array of read noise estimates
-	readnoise_array = []
-	# initialize the sample counter
-        count = 0
-	while(count<1000): # sample 1000 sub-images for the fits file
-		# sample sub-images of size Npx by Npx for random locations in the fits file image image
-		x = np.random.randint(xmin+Npx, xmax-Npx)
-		y = np.random.randint(ymin+Npx, ymax-Npx)
-		r, d = bigw.wcs_pix2world(x,y, 1)
-		img = sub_image(r, d, bigw, hdulist, Npx)
-		
-		# Only accept sub-images if they look like Gaussian noise.
-		# Cuts on kurtosis, skewness, and maximum value compared to median were determined from simulated Gaussian distributions. The values are selected based on the bounds that reteain 99.9% of the simulated data.
-		if(scipy.stats.kurtosis(np.ravel(img))<-0.42 or scipy.stats.kurtosis(np.ravel(img))>0.66 or np.abs(scipy.stats.skew(np.ravel(img)))>0.26 or np.max(img)>np.median(img)+4.5*np.sqrt(np.median(img))):
-		    continue
-		# Next we check for gradient effects. We divide the sub-image into quadrants and estimate their means and rms.
-		# We find the maximum difference between means divided by the root-sum-squared of their standard deviations.
-		# Finally we cut on the maximum value. This value was determined from a simulation of Guassian noise and finding the bound that retained 99.9% of the data.
-		mQ1 = np.mean(img[:(len(img)-1)/2, :(len(img)-1)/2])
-		mQ2 = np.mean(img[:(len(img)-1)/2, (len(img)-1)/2:])
-		mQ3 = np.mean(img[(len(img)-1)/2:, :(len(img)-1)/2])
-		mQ4 = np.mean(img[(len(img)-1)/2:, (len(img)-1)/2:])
-		rQ1 = rms(img[:(len(img)-1)/2, :(len(img)-1)/2])
-		rQ2 = rms(img[:(len(img)-1)/2, (len(img)-1)/2:])
-		rQ3 = rms(img[(len(img)-1)/2:, :(len(img)-1)/2])
-		rQ4 = rms(img[(len(img)-1)/2:, (len(img)-1)/2:])
-		normed_quad_diffs = [ (mQ1-mQ2)/np.sqrt(rQ1**2 + rQ2**2), (mQ1-mQ3)/np.sqrt(rQ1**2 + rQ3**2), (mQ1-mQ4)/np.sqrt(rQ1**2 + rQ4**2), (mQ2-mQ3)/np.sqrt(rQ2**2 + rQ3**2), (mQ2-mQ4)/np.sqrt(rQ2**2 + rQ4**2), (mQ3-mQ4)/np.sqrt(rQ3**2 + rQ4**2)]
-		if( np.max(np.abs(normed_quad_diffs)) > 0.245):
-			continue
+    # get valid image limits
+    xmin = int((hdulist[0].header['TRIMSEC'])[1:-1].split(',')[0].split(':')[0])
+    xmax = int((hdulist[0].header['TRIMSEC'])[1:-1].split(',')[0].split(':')[1])
+    ymin = int((hdulist[0].header['TRIMSEC'])[1:-1].split(',')[1].split(':')[0])
+    ymax = int((hdulist[0].header['TRIMSEC'])[1:-1].split(',')[1].split(':')[1])
+    # initialize the array of read noise estimates
+    readnoise_array = []
+    # initialize the sample counter
+    count = 0
+    trials=0
+    while(count<1000): # sample 1000 sub-images for the fits file
+        # if no acceptable noise estimates are found below, then bail after 20000 trials
+        trials += 1
+        if(trials>20000):
+            return -1
+        # sample sub-images of size Npx by Npx for random locations in the fits file image image
+        x = np.random.randint(xmin+Npx, xmax-Npx)
+        y = np.random.randint(ymin+Npx, ymax-Npx)
+        r, d = bigw.wcs_pix2world(x,y, 1)
+        img = sub_image(r, d, bigw, hdulist, Npx)
 
-		# histogram the values of the image
-		h,b = np.histogram(img.flat, bins=30)
-		v=b[:-1]+(b[1]-b[0])/2.
-		# Fit a Gaussian, if the fit fails, then don't accept this trial as a noise sample.
-		try:
-			p0 = [np.max(h), np.mean(img), np.sqrt( np.mean(img**2)-np.mean(img)**2 )]
-			popt, pcov = curve_fit(gaussian, (v), h, p0=p0)
-		except:
-			continue
-		if(popt[0]<=0. or popt[1]<0):
-			continue
-		if(popt[1]**2 - popt[2] <= 0.):
-			continue
-		# estimate the readnoise for this subimage based on a Gaussian fit.
-		sigma_CCD = popt[2]
-		mean_CCD  = popt[1]
-		gain = float(hdulist[0].header['GAIN'])
+        # Only accept sub-images if they look like Gaussian noise.
+        # Cuts on kurtosis, skewness, and maximum value compared to median were determined from simulated Gaussian distributions. The values are selected based on the bounds that reteain 99.9% of the simulated data.
+        if(scipy.stats.kurtosis(np.ravel(img))<-0.42 or scipy.stats.kurtosis(np.ravel(img))>0.66 or np.abs(scipy.stats.skew(np.ravel(img)))>0.26 or np.max(img)>np.median(img)+4.5*np.sqrt(np.median(img))):
+            continue
+        # Next we check for gradient effects. We divide the sub-image into quadrants and estimate their means and rms.
+        # We find the maximum difference between means divided by the root-sum-squared of their standard deviations.
+        # Finally we cut on the maximum value. This value was determined from a simulation of Guassian noise and finding the bound that retained 99.9% of the data.
+        mQ1 = np.mean(img[:(len(img)-1)/2, :(len(img)-1)/2])
+        mQ2 = np.mean(img[:(len(img)-1)/2, (len(img)-1)/2:])
+        mQ3 = np.mean(img[(len(img)-1)/2:, :(len(img)-1)/2])
+        mQ4 = np.mean(img[(len(img)-1)/2:, (len(img)-1)/2:])
+        rQ1 = rms(img[:(len(img)-1)/2, :(len(img)-1)/2])
+        rQ2 = rms(img[:(len(img)-1)/2, (len(img)-1)/2:])
+        rQ3 = rms(img[(len(img)-1)/2:, :(len(img)-1)/2])
+        rQ4 = rms(img[(len(img)-1)/2:, (len(img)-1)/2:])
+        normed_quad_diffs = [ (mQ1-mQ2)/np.sqrt(rQ1**2 + rQ2**2), (mQ1-mQ3)/np.sqrt(rQ1**2 + rQ3**2), (mQ1-mQ4)/np.sqrt(rQ1**2 + rQ4**2), (mQ2-mQ3)/np.sqrt(rQ2**2 + rQ3**2), (mQ2-mQ4)/np.sqrt(rQ2**2 + rQ4**2), (mQ3-mQ4)/np.sqrt(rQ3**2 + rQ4**2)]
+        if( np.max(np.abs(normed_quad_diffs)) > 0.245):
+            continue
 
-		# If the readnoise calculation is going to fail, then continue
-		if(sigma_CCD**2 < mean_CCD/gain):
-			continue
-		rn = np.sqrt(sigma_CCD**2 - mean_CCD/gain)
-		#print rn
-	        # increment counter only if image is accepted, the gaussian fit is successful, and the readnoise estimate is sensible	
-	        count += 1
-		readnoise_array.append(rn)
+        # histogram the values of the image
+        h,b = np.histogram(img.flat, bins=30)
+        v=b[:-1]+(b[1]-b[0])/2.
+        # Fit a Gaussian, if the fit fails, then don't accept this trial as a noise sample.
 
-	# convert readnoise_array from a list to an array
-	readnoise_array = np.array(readnoise_array)
-	# estimate the readnoise by using the median of the distribution
-	readnoise = np.median(readnoise_array)
-	#print np.min(readnoise_array), np.max(readnoise_array)
-	#print np.median(readnoise_array), np.mean(readnoise_array), np.sqrt(np.mean(readnoise_array**2) - np.mean(readnoise_array)**2)
-	if(display):
-		plt.figure(figsize=(9,7))
-		plt.subplot(221)
-		plt.imshow(img, interpolation='none')
-		plt.colorbar()
-		plt.title('Random Image Portion')
-		plt.xlabel('pixel')
-		plt.ylabel('pixel')
-		plt.subplot(222)
-		plt.step(v,h, lw=2)
-		plt.plot(v,gaussian(v,*popt), 'r--', lw=2) 
-		plt.title('Distribution of Counts\nw/ Gaussian Fit')
-		plt.xlabel('CCD Counts (gain corrected)')
-		plt.subplot(212)
-		print 'median read noise', np.median(readnoise_array)
-		a,b,c = plt.hist(readnoise_array, bins=int(np.max(readnoise_array)-np.min(readnoise_array)))
-		plt.plot([np.median(readnoise_array), np.median(readnoise_array)],[0., 1.2*max(a)],'r-', lw=2, label='median')
-		plt.legend(loc=1)
-		plt.ylim(0.,1.2*max(a))
-		plt.xlabel('Read Noise, CCD counts (gain corrected)')
-		plt.title('Multiple Read Noise Estimates')
-		plt.suptitle(out.split('/')[-1], fontsize=20)
-		plt.subplots_adjust(top=0.85, wspace=0.3, hspace=0.4)
-		#plt.savefig(out+'.png', dpi=50)
-		plt.savefig(out+'.pdf')
-		#plt.show()
-	return readnoise
+        '''
+        plt.figure()
+        plt.imshow(hdulist[0].data, interpolation='none')
+        plt.colorbar()
+        plt.figure()
+        plt.subplot(121)
+        plt.imshow(img, interpolation  = 'none')
+        plt.colorbar()
+        plt.subplot(122)
+        plt.step(v,h,where='mid')
+        plt.show()
+        '''
+        try:
+            p0 = [np.max(h), np.mean(img), np.sqrt( np.mean(img**2)-np.mean(img)**2 )]
+            popt, pcov = curve_fit(gaussian, (v), h, p0=p0)
+        except:
+            continue
+        if(popt[0]<=0. or popt[1]<0):
+            continue
+        if(popt[1]**2 - popt[2] <= 0.):
+            continue
+        # estimate the readnoise for this subimage based on a Gaussian fit.
+        sigma_CCD = popt[2]
+        mean_CCD  = popt[1]
+        gain = float(hdulist[0].header['GAIN'])
+
+        # If the readnoise calculation is going to fail, then continue
+        if(sigma_CCD**2 < mean_CCD/gain):
+            continue
+        rn = np.sqrt(sigma_CCD**2 - mean_CCD/gain)
+        #print rn
+        # increment counter only if image is accepted, the gaussian fit is successful, and the readnoise estimate is sensible	
+        count += 1
+        readnoise_array.append(rn)
+
+    # convert readnoise_array from a list to an array
+    readnoise_array = np.array(readnoise_array)
+    # estimate the readnoise by using the median of the distribution
+    readnoise = np.median(readnoise_array)
+    #print np.min(readnoise_array), np.max(readnoise_array)
+    #print np.median(readnoise_array), np.mean(readnoise_array), np.sqrt(np.mean(readnoise_array**2) - np.mean(readnoise_array)**2)
+    if(display):
+        plt.figure(figsize=(9,7))
+        plt.subplot(221)
+        plt.imshow(img, interpolation='none')
+        plt.colorbar()
+        plt.title('Random Image Portion')
+        plt.xlabel('pixel')
+        plt.ylabel('pixel')
+        plt.subplot(222)
+        plt.step(v,h, lw=2)
+        plt.plot(v,gaussian(v,*popt), 'r--', lw=2) 
+        plt.title('Distribution of Counts\nw/ Gaussian Fit')
+        plt.xlabel('CCD Counts (gain corrected)')
+        plt.subplot(212)
+        print 'median read noise', np.median(readnoise_array)
+        a,b,c = plt.hist(readnoise_array, bins=int(np.max(readnoise_array)-np.min(readnoise_array)))
+        plt.plot([np.median(readnoise_array), np.median(readnoise_array)],[0., 1.2*max(a)],'r-', lw=2, label='median')
+        plt.legend(loc=1)
+        plt.ylim(0.,1.2*max(a))
+        plt.xlabel('Read Noise, CCD counts (gain corrected)')
+        plt.title('Multiple Read Noise Estimates')
+        plt.suptitle(out.split('/')[-1], fontsize=20)
+        plt.subplots_adjust(top=0.85, wspace=0.3, hspace=0.4)
+        #plt.savefig(out+'.png', dpi=50)
+        plt.savefig(out+'.pdf')
+        #plt.show()
+    return readnoise
 
     # Use the read noise and the image data to make a noise map
 
